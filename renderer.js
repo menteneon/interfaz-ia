37464bc294c9af733e569ad5538895dc4edf4213
ssofiasandoval <sofia.sandoval@mail.udp.cl>
2023-10-13 11:22:47 -0300

    agregar sliders

--- renderer.js
+++ renderer.js
@@ -5,10 +5,32 @@
  * `contextIsolation` is turned on. Use the contextBridge API in `preload.js`
  * to expose Node.js functionality from the main process.
  */
-let slider = document.getElementById("myRange");
-let output = document.getElementById("demo");
-output.innerHTML = slider.value;
+// slider horizontal
+let sliderHorizontal = document.getElementById("sliderh");
+let number = document.getElementById("number");
 
-slider.oninput = function() {
-  output.innerHTML = this.value;
+
+// cuando hay un cambio desde el slider va cambiar el valor 1,2... callback es cuando un elemento ocurre 
+sliderHorizontal.oninput = function() {
+  number.value = this.value;
 }
+//cuando hay un cambio desde el input el slider cambiara 
+number.oninput = function(){
+  sliderHorizontal.value = this.value;
+}
+
+
+//slider vertical
+let sliderVertical = document.getElementById("sliderv");
+let numbertwo = document.getElementById("numbertwo");
+
+// cuando hay un cambio desde el slider va cambiar el valor 1,2...
+sliderVertical.oninput = function() {
+  numbertwo.value = this.value;
+}
+//cuando hay un cambio desde el input el slider cambiara 
+numbertwo.oninput = function(){
+  sliderVertical.value = this.value;
+}
+
+
