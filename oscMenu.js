b49a54f16b199beeb6ca85c3da1e5e004dda1d2e
aarón montoya-moraga <montoyamoraga@gmail.com>
2023-11-13 11:56:52 -0300

    borrar codigo redundante en oscMenu

--- oscMenu.js
+++ oscMenu.js
@@ -3,6 +3,7 @@
 //no funciona 
 // const { ipcRenderer } =  require('electron');
 
+
 // Obtener el botón por su ID
 const botonConectarOSC = document.getElementById('botonConectarOSC');
 
@@ -15,16 +16,25 @@ botonConectarOSC.addEventListener('click', () => {
 });
 //no funciona 
 
+// obtener el elemento input de puerto de envio OSC por su id
+const inputPuertoEnvio = document.getElementById('puertoEnvio');
+
+let puertoEnvio = 8001;
+
+// TODO: agregar un event listener que haga que
+// cuando cambia el numero del puerto y hacemos ENTEr
+// se actualice la variable puertoEnvio
+// inputPuertoEnvio.addEventListener()
 
 
-//Esto si funciona 
+//Esto si funciona
 const { Client } = require("osc-js");
 
 const oscClient = new Client({
   send: {
     host: "127.0.0.1", // Dirección IP del servidor OSC (puede ser la misma máquina si Protokol
     // y tu aplicación están en la misma computadora)
-    port: 12345 // Puerto del servidor OSC (debe coincidir con el puerto que Protokol está escuchando)
+    port: puertoEnvio // Puerto del servidor OSC (debe coincidir con el puerto que Protokol está escuchando)
   }
 });
 
@@ -33,46 +43,14 @@ oscClient.send(new OSC.Message("/ruta/osc", 123, "hola"));
 //hasta aqui :D
 
 
-
-
 //no se donde va 
-const { app, BrowserWindow, ipcMain } = require('electron');
-const path = require('path');
-
-let mainWindow;
-
-function createWindow() {
-  mainWindow = new BrowserWindow({
-    width: 800,
-    height: 600,
-    webPreferences: {
-      preload: path.join(__dirname, 'preload.js'),
-      nodeIntegration: true
-    }
-  });
-
-  // Cargar el archivo HTML en la ventana
-  mainWindow.loadFile('index.html');
-
-  // Escuchar el evento desde el proceso de renderizado
-  ipcMain.on('conectar-osc', () => {
-    // Lógica para conectar OSC aquí
-    console.log('Conectar OSC...');
-    // Tu lógica para conectar OSC va aquí
-  });
-
-  mainWindow.on('closed', function () {
-    mainWindow = null;
-  });
-}
-
-app.whenReady().then(createWindow);
-
-app.on('window-all-closed', function () {
-  if (process.platform !== 'darwin') app.quit();
-});
-
-app.on('activate', function () {
-  if (mainWindow === null) createWindow();
-});
+// const { app, BrowserWindow, ipcMain } = require('electron');
+// const path = require('path');
+// let mainWindow;
+  // // Escuchar el evento desde el proceso de renderizado
+  // ipcMain.on('conectar-osc', () => {
+  //   // Lógica para conectar OSC aquí
+  //   console.log('Conectar OSC...');
+  //   // Tu lógica para conectar OSC va aquí
+  // });
 //hasta aqui 
